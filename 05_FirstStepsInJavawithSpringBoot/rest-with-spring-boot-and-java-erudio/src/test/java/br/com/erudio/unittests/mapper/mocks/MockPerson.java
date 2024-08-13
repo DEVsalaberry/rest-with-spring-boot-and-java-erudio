5ff9b86f22d8dc363c2cc60d99661926f13cd0d0
DEVsalaberry <felipesalaberrydevv@gmail.com>
2024-08-13 17:07:48 -0300

    Adiciona configuração do OpenAPI para documentação da API e ajustes em PersonServices e RequiredObjectIsNullException
    - Cria configuração do OpenAPI para documentação da API
    - Implementa testes unitários para PersonServices com Mockito
    - Atualiza métodos em PersonServices para adicionar links HATEOAS e tratamento de exceções
    - Ajusta RequiredObjectIsNullException para mensagem padrão

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mapper/mocks/MockPerson.java
@@ -0,0 +1,30 @@
+package br.com.erudio.unittests.mapper.mocks;
+
+import java.util.List;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.model.Person;
+
+public class MockPerson {
+
+	public Person mockEntity(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PersonVO mockVO(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Person> mockEntityList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Person mockEntity1(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
